bac71c5ff78a31f098f45954d9702766e3d343a4
adoppp <chdn786od@gmail.com>
2025-07-22 23:08:39 +0200

    feature(routing): routing added

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,9 +1,20 @@
-import App from "../App";
 import { createBrowserRouter } from "react-router";
+
+import { RootLayout } from "../layouts/RootLayout/RootLayout";
+import { RouterConfig } from "./router.config";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />
+        element: <RootLayout />,
+        children: RouterConfig.map(({ index, path, element: Component}) => {
+            return (
+                {
+                    path,
+                    index,
+                    element: <Component />
+                }
+            )
+        }),
     }
 ]);

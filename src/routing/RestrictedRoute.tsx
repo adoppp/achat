800eb39ec172bef4483a04f3ee640a750622c86d
Daniil Cheban <chdn786od@gmail.com>
2025-08-12 20:45:13 +0200

    feature(restricted route): types for component added

--- src/routing/RestrictedRoute.tsx
+++ src/routing/RestrictedRoute.tsx
@@ -1,5 +1,6 @@
+import type { FC, ReactElement } from "react";
 import { Navigate } from "react-router";
 
-export const RestrictedRoute = () => {
+export const RestrictedRoute: FC = (): ReactElement => {
     return <Navigate to='chats' />;
 };

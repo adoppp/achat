01eb78fd496679fc53d009d179d83016192e26f9
Daniil Cheban <chdn786od@gmail.com>
2025-08-14 12:39:08 +0200

    refactoring(messages list): item redering moved to hook, styles to empty list added

--- src/sections/Chats/ChatMessages/MessagesList/MessagesList.hooks.tsx
+++ src/sections/Chats/ChatMessages/MessagesList/MessagesList.hooks.tsx
@@ -1,16 +1,39 @@
 import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { format } from "date-fns";
+import classNames from "classnames/bind";
+
+import styles from '@/sections/Chats/ChatMessages/MessagesList/MessagesList.module.scss';
 
 import { firestore } from "@/services";
 import type { Message } from "@/types";
 import { useAuth } from "@/utils/useAuth";
+
+const cn = classNames.bind(styles);
 
 export const useMessagesList = () => {
     const { chatId } = useParams<{ chatId: string }>();
     const [messages, setMessages] = useState<Message[]>([]);
     const { user } = useAuth();
     const listRef = useRef<HTMLUListElement>(null); 
+
+    const messageItems = messages.map(message => {
+            const time = message.timeStamp ? format(new Date(message.timeStamp.seconds * 1000), 'HH:mm') : '';
+            return (    
+                <li key={message.id} className={cn('message', message.senderId === user?.uid ? 'message__sender' : 'message__receiver')}>
+                    <p>
+                        {message.text}
+                    </p>
+                    <span>
+                        {time}
+                        <div>
+                            {time}
+                        </div>
+                    </span>
+                </li>
+            );
+        });
 
     useEffect(() => {
         if (!chatId) return;
@@ -38,5 +61,5 @@ export const useMessagesList = () => {
         }
     }, [messages]);
 
-    return { listRef, messages, user };
+    return { listRef, messageItems };
 };
--- src/sections/Chats/ChatMessages/MessagesList/MessagesList.tsx
+++ src/sections/Chats/ChatMessages/MessagesList/MessagesList.tsx
@@ -1,38 +1,24 @@
+import type { FC, ReactElement } from "react";
 import classNames from "classnames/bind";
-import { format } from "date-fns";
 
 import styles from '@/sections/Chats/ChatMessages/MessagesList/MessagesList.module.scss';
 
 import { useMessagesList } from "@/sections/Chats/ChatMessages/MessagesList/MessagesList.hooks";
 
-
 const cn = classNames.bind(styles);
 
-export const MessagesList = () => {    
-    const { listRef, messages, user } = useMessagesList();
+export const MessagesList: FC = (): ReactElement => {    
+    const { listRef, messageItems } = useMessagesList();
 
     return (
-        <ul className={cn('messages')} ref={listRef}>
+        <>
             {
-                messages.length !== 0 ?
-                messages.map(message => {
-                    const time = message.timeStamp ? format(new Date(message.timeStamp.seconds * 1000), 'HH:mm') : '';
-                    return (    
-                        <li key={message.id} className={cn('message', message.senderId === user?.uid ? 'message__sender' : 'message__receiver')}>
-                            <p>
-                                {message.text}
-                            </p>
-                            <span>
-                                {time}
-                                <div>
-                                    {time}
-                                </div>
-                            </span>
-                        </li>
-                    );
-                }) :
-                <h1>No messages yet</h1>
+                messageItems.length !== 0 ?
+                <ul className={cn('messages')} ref={listRef}>
+                    {messageItems}
+                </ul> :
+                <h1 className={cn('messages__empty')}>No messages yet</h1>
             }
-        </ul>
+        </>
     );
 };

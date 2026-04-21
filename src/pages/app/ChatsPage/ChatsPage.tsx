02d6140958ceb361acf48f474f6e02eb176b1001
Daniil Cheban <chdn786od@gmail.com>
2026-04-21 21:24:51 +0200

    Ui button added; theme has accent colors that deriven by "--accent" variable that has rgb numbers(reusable in rgb function and dynamic alpha) -> color states extending from this = one color for all variants(opacity differrence); transition updated

--- src/pages/app/ChatsPage/ChatsPage.tsx
+++ src/pages/app/ChatsPage/ChatsPage.tsx
@@ -1,7 +1,51 @@
-import type { FC } from 'react';
+import { 
+    // useState, 
+    type FC 
+} from 'react';
+// import classNames from 'classnames/bind';
+
+// import { Button } from '@/ui/Button/Button';
+
+// import styles from '@/pages/app/ChatsPage/ChatsPage.module.scss';
+
+// const cn = classNames.bind(styles);
 
 const ChatsPage: FC = () => {
-    return <div>Select a chat</div>;
+    // const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    // const handleOnClickTest = () => {
+    //     setIsLoading(true);
+
+    //     setTimeout(() => {
+    //         setIsLoading(false);
+    //     }, 5000);
+    // }
+
+    return (
+        <div>
+            Select a chat
+            {/* <Button 
+                isLoading={isLoading} 
+                onClick={handleOnClickTest}
+                size='s'
+            >
+                Click1
+            </Button>
+            <Button 
+                isLoading={isLoading} 
+                onClick={handleOnClickTest}
+                variant='secondary'
+            >
+                Click2
+            </Button>
+            <Button 
+                isDisabled 
+                onClick={handleOnClickTest}
+            >
+                Click3
+            </Button> */}
+        </div>
+    );
 };
 
 export default ChatsPage;
---
+++ src/ui/Button/Button.tsx
@@ -0,0 +1,56 @@
+import type { ButtonHTMLAttributes, FC, ReactNode } from 'react';
+import classNames from 'classnames/bind';
+
+import styles from '@/ui/Button/Button.module.scss';
+
+type Size = 's' | 'm' | 'l';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: 'primary' | 'secondary',
+    size?: Size,
+    customClassName?: string,
+
+    isLoading?: boolean,
+    isDisabled?: boolean,
+
+    leftIcon?: ReactNode,
+    rightIcon?: ReactNode
+};
+
+const cn = classNames.bind(styles);
+
+export const Button: FC<ButtonProps> = ({
+    variant = 'primary',
+    size = 'm',
+    customClassName,
+    isLoading = false,
+    isDisabled = false,
+    leftIcon,
+    rightIcon, 
+    children,
+    ...props 
+}) => {
+    const isButtonDisabled = isDisabled || isLoading;
+
+    return(
+        <button
+            className={cn(
+                'default',
+                variant,
+                isLoading && 'loading',
+                size,
+                customClassName
+            )}
+            disabled={isButtonDisabled}
+            {...props}
+        >
+            <span className={cn('content', isLoading && 'hidden')}>
+                {leftIcon}
+                {children}
+                {rightIcon}
+            </span>
+
+            {isLoading && <span className={cn('loader')} />}
+        </button>
+    );
+};

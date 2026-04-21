9b84e231dc9be05f1017726bbbf0322ac09ca0ab
Daniil Cheban <chdn786od@gmail.com>
2026-04-21 12:29:48 +0200

    Baisc styles added; mixins and etc. added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
+
 import { router } from '@/routing';
+import '@/styles/global.scss';
+import '@/styles/reset.scss';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
